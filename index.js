68c88b032852ded9a037c9ae4d46bdbeb352ade8
Kristina Schneider <me@kristinaschneider.com>
2016-11-16 16:19:06 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const mount = require('choo/mount')
+const css = require("sheetify")
+const html = require("bel")
+
+css('dat-colors')
+css('tachyons')
+
+css`
+  .gray {
+    color: var(--color-neutral--80);
+  }
+`
+
+const footer1 = css`
+  :host {
+    background-color: var(--color-green);
+  }
+`
+
+var tree = html`
+  <body>
+    <header><a>button</a></header>
+    <main>
+      <h1>Hello World</h1>
+      <img src="" />
+    </main>
+    <footer class=${footer1}>stuff</footer>
+  </body>
+`
+
+// replace body with tree
+mount('body', tree)
